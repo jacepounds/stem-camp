a28a911ba5f1b2314d87331bb8ca4bfb7eee5878
jacepounds <jacepounds@gmail.com>
2017-06-12 11:22:11 -0500

    Add files via upload

---
+++ hello-world/Hello World.c
@@ -0,0 +1,13 @@
+/*
+ *Jace Kalvin Pounds
+ * Program #1
+ * Hello World
+ * 06/12/17
+*/
+#include "simpletools.h"                      // Include simple tools
+
+int main()                                    // Main function
+{
+  print("Math is so much fun \n"); 
+  print("Jace Kalvin Pounds"); 
+}
